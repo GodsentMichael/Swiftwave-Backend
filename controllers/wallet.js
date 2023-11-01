d7680ecea39a96ccf64ec4766cb3dae4ab980b96
Godsent Michael <gentlemotivator@gmail.com>
2023-11-01 23:37:22 +0100

    Done: Create wallet and set wallet pin functionality

--- controllers/wallet.js
+++ controllers/wallet.js
@@ -1,28 +1,55 @@
-const Wallet = require('models/Wallet')
-const User = require('models/User')
-const {WalletSchema} = require("validations/wallet")
+const Wallet = require("models/Wallet");
+const User = require("models/User");
+const { WalletSchema } = require("validations/wallet");
 const { encrypt } = require("helpers/auth");
 // const WalletTransaction = require("models/WalletTransactionSchema")
 
 // THIS WALLET CONTROLLER SHOULD BE TRIGGERED AS SOON AS THE USER IS CREATED
-exports.createWallet = async(req, res)=>{
-    try {
-        const { id } = req.user
-        const user = await User.findById(id)
-        console.log("USER=>", user)
-        const wallet = await Wallet.findOne({ user: user.id })
-        console.log("WALLET=>", wallet)
+exports.createWallet = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const user = await User.findById(id);
+    console.log("USER=>", user);
+    const wallet = await Wallet.findOne({ user: user.id });
 
+    if (!user) return res.status(400).json({ error: "User not found" });
+    if (wallet) return res.status(400).json({ error: "Wallet already exists" });
 
-        if(!user) return res.status(400).json({error: "User not found"})
-        if(wallet) return res.status(400).json({error: "Wallet already exists"})
+    const createdWallet = await Wallet.create({
+      user: user.id,
+    });
+    res.status(200).json({ message: "Wallet Created Succesully", createdWallet });
+  } catch (error) {
+    console.log("ERROR=>", error);
+    res.status(500).json(error);
+  }
+};
 
-        const createdWallet = await Wallet.create({
-            user: user.id,
-        })
-        res.status(200).json({ message: "Wallet Created", createdWallet})
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error)
+exports.createPin = async (req, res) => {
+  const { id } = req.user;
+  const body = WalletSchema.safeParse(req.body);
+
+  if (!body.success) return res.status(400).json({ errors: body.error.issues });
+
+  try {
+    const wallet = await Wallet.findOne({ user: id });
+    if (!wallet) return res.status(400).json({ error: "Wallet not found" });
+
+    if (wallet.isPinSet) {
+      return res.status(400).json({ error: "Pin was already set" });
+    } else {
+      const { pin } = body.data;
     }
-}
+
+    if (!wallet.isPinSet) {
+      const hashedPin = await encrypt(body.data.pin);
+      wallet.pin = hashedPin;
+      wallet.isPinSet = true;
+      await wallet.save();
+      return res.status(200).json({ message: "Pin Created Succesfully", wallet });
+    }
+  } catch (error) {
+    console.log("ERROR=>", error);
+    res.status(500).json(error);
+  }
+};
--- routes/api/wallet.routes.js
+++ routes/api/wallet.routes.js
@@ -1,5 +1,5 @@
 const {
-    createWallet
+    createWallet, createPin
   } = require("controllers/wallet");
 const { isAuthenticated } = require("middlewares/auth");
 const express = require("express");
@@ -9,7 +9,12 @@ const router = Router();
 
 // route POST api/wallet/create
 // desc  create wallet
-// access public
+// access private
 router.post("/create", isAuthenticated,createWallet);
+
+// route POST api/wallet/create-pin
+// desc  create pin
+// access private
+router.post("/create-pin", isAuthenticated, createPin)
 
 module.exports = router;
